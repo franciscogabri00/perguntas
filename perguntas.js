4b0856e101d63fe923a96a7463d35fd523e6f843
franciscogabri00 <166419351+franciscogabri00@users.noreply.github.com>
2024-11-04 12:24:59 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'HTML é uma linguagem de marcação utilizada na construção de páginas na Web.'
+)
+
+criaCartao(
+    'Filme',
+    'Qual o filme com maior bilheteria do mundo',
+    'Avatar'
+)
+
+criaCartao(
+    'Jogos',
+    'Qual ano foi lançado Minecraft?',
+    '2011'
+)
+
+criaCartao(
+    'filme',
+    'filme mais assistido do mundo 2024',
+    'DUNA parte 2'
+)
+
+criaCartao(
+    'Matemática II',
+    'Qual aplicativo utilizamos nas aulas de Programação - Matemática II?',
+    'Visual Studio Code'
+)
